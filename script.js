109613b8b962f96d8335c74317a7394a8845866e
Minh_Nguyen <dminhnguyen161@gmail.com>
2020-09-11 18:26:17 +0700

    added delete and retweet

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let currentUser = "Lukas";
 let uniqueID = 0;
 
 let render = () => {
-<<<<<<< HEAD
   tweets.sort(function (a, b) {
     // Turn your strings into dates, and then subtract them
     // to get a value that is either negative, positive, or zero.
@@ -72,30 +71,6 @@ let render = () => {
   });
   document.getElementById("feed").innerHTML = tweetHTML;
 };
-=======
-    let tweetHTML = tweets.map((item)=>`
-     <div class="card border-success mb-3">
-                <div class="card-header bg-transparent border-success">
-                    <img src="https://storage.pixteller.com/designs/designs-images/2016-11-19/02/thumbs/img_page_1_58305b35ebf5e.png"/>
-                    <div>UserName</div>
-                    <div>time post</div>
-                </div>
-                <div class="card-body text-success">
-                  <h5 class="card-title"></h5>
-                  <p class="card-text">${item.content}</p>
-                </div>
-                <div class="card-footer bg-transparent border-success">
-                    <i class="fas fa-comment"></i>
-                    <i class="fas fa-retweet"></i>
-                    <i class="far fa-heart"></i>
-                    <i class="far fa-edit"></i>
-                </div>
-              </div>
-    `) 
-    document.getElementById("feed").innerHTML = tweetHTML
-    console.log(tweets)
-}   
->>>>>>> da5bd3a7e4239d9d1f9f74da2ccc3cf1b14af301
 
 let getHashTag = (text) => {};
 
@@ -134,7 +109,6 @@ let deleteTweet = (id) => {
 };
 
 let postTweet = () => {
-<<<<<<< HEAD
   console.log("tweeted");
   uniqueID++;
   let text = document.getElementById("postInput").value;
@@ -183,33 +157,6 @@ let renderComment = () => {};
 let postComment = () => {
   let commentContent = document.getElementById("");
 };
-=======
-    console.log("tweeted")
-    let text = document.getElementById("postInput").value;
-    let postUser = currentUser;
-    let postHashTag = getHashTag(text);
-    let tweetObject = {
-        user: postUser,
-        content: text,
-        likes: 0,
-        time: Date.now(),
-        isRetweet: false,
-        hastag: postHashTag,
-        comment: [] 
-    };
-    tweets.push(tweetObject);
-    console.log(tweets)
-    render();
-}
-
-let renderComment = () => {
-
-}
-
-let postComment = () => {
-    let commentContent = document.getElementById("")
-}
->>>>>>> da5bd3a7e4239d9d1f9f74da2ccc3cf1b14af301
 
 document.getElementById("wordCount").innerHTML = 140;
 let count = () => {
