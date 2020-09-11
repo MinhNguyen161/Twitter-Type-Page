30627539699810b8f8ae7c755e566450fce12a58
linhle <lklinh@outlook.com>
2020-09-11 10:30:26 +0700

    add wordcount

--- script.js
+++ script.js
@@ -1,39 +1,38 @@
 let tweets = [];
 
-
-
 let render = () => {
-    return;
-}
+  return;
+};
 render();
 
-let getHashTag = (text) => {
-
-}
+let getHashTag = (text) => {};
 
 let postTweet = () => {
-    let text = document.getElementById("postInput").value;
-    let postUser = currentUser;
-    let postHashTag = getHashTag(text);
-    let tweetObject = {
-        user: postUser,
-        content: text,
-        likes: 0,
-        time: "",
-        isExist: true,
-        hastag: postHashTag,
-        comment: []
-    };
-    tweets.push(tweetObject);
-    render();
-}
+  let text = document.getElementById("postInput").value;
+  let postUser = currentUser;
+  let postHashTag = getHashTag(text);
+  let tweetObject = {
+    user: postUser,
+    content: text,
+    likes: 0,
+    time: "",
+    isExist: true,
+    hastag: postHashTag,
+    comment: [],
+  };
+  tweets.push(tweetObject);
+  render();
+};
 
-let renderComment = () => {
-
-}
+let renderComment = () => {};
 
 let postComment = () => {
-    let commentContent = document.getElementById("")
-}
+  let commentContent = document.getElementById("");
+};
 
-
+document.getElementById("wordCount").innerHTML = 140;
+let count = () => {
+  let input = document.getElementById("postInput").value;
+  let inputLetter = input.split("");
+  document.getElementById("wordCount").innerHTML = 140 - inputLetter.length;
+};
